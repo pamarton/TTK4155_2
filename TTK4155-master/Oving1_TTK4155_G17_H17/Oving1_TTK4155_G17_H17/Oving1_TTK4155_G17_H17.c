27faaa217f5a85a9c4686a3f27e2aec918f717f7
pamarton <pamarton@stud.ntnu.no>
2017-09-11 15:55:42 +0200

    Add files via upload

---
+++ TTK4155-master/Oving1_TTK4155_G17_H17/Oving1_TTK4155_G17_H17/Oving1_TTK4155_G17_H17.c
@@ -0,0 +1,77 @@
+/*
+ * Oving1_TTK4155_G17_H17.c
+ *
+ * Created: 04.09.2017 11:50:29
+ *  Author: pamarton
+ */ 
+#include <stdio.h>
+#define F_CPU 4915200 // clock frequency in Hz
+#define BAUDRATE 9600 // Valgt verdi data sendt pr sekund
+#define UBBR 32 - 1 //F_CPU/(16*BAUDRATE)-1 //USART BAUD RATE REGISTER VALUE
+#include <avr/io.h>
+//#include <util/delay.h>
+//#include <avr/interrupt.h>
+#include "uart.h"
+#include "i_o.h"
+
+void SRAM_test(void);
+
+int main(void)
+{
+	init_UART(UBBR);
+	BIT_ON(MCUCR,SRE);
+	
+	
+	DDRA = 0xFF;
+	BIT_OFF_A(0);
+	BIT_ON_A(1);
+	
+	
+	
+	BIT_ON(DDRE,1);
+	BIT_ON(PORTE,1);
+	BIT_OFF(PORTE,1);
+	
+	SRAM_test();
+	
+	testUart();
+	while (1)
+	{
+		printf("placeholder\n");
+	}
+}
+
+#include <stdlib.h>
+void SRAM_test(void)
+{
+	volatile char *ext_ram = (char *) 0x1800; // Start address for the SRAM
+	uint16_t ext_ram_size = 0x800;
+	uint16_t write_errors = 0;
+	uint16_t retrieval_errors = 0;
+	printf("Starting SRAM test...\n");
+	// rand() stores some internal state, so calling this function in a loop will
+	// yield different seeds each time (unless srand() is called before this function)
+	uint16_t seed = rand();
+	// Write phase: Immediately check that the correct value was stored
+	srand(seed);
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		ext_ram[i] = some_value;
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {
+			printf("Write phase error: ext_ram[%4d] = %02X (should be %02X)\n", i,	retreived_value, some_value);
+			write_errors++;
+		}
+	}
+	// Retrieval phase: Check that no values were changed during or after the writephase
+	srand(seed);
+	// reset the PRNG to the state it had before the write phase
+	for (uint16_t i = 0; i < ext_ram_size; i++) {
+		uint8_t some_value = rand();
+		uint8_t retreived_value = ext_ram[i];
+		if (retreived_value != some_value) {printf("Retrieval phase error: ext_ram[%4d] = %02X (should be %02X)\n",i, retreived_value, some_value);
+			retrieval_errors++;
+		}
+	}
+	printf("SRAM test completed with\n%4d errors in write phase and\n%4d errors	in retrieval phase\n\n", write_errors, retrieval_errors);
+}
